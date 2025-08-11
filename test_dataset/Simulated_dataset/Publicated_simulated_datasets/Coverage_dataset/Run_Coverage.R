7ec370748dcaafb5ed395440e856bf529e35cd8e
BowenArchaman <zgl19970514@outlook.com>
2025-08-11 10:08:28 +0100

    update simulated dataset

---
+++ test_dataset/Simulated_dataset/Publicated_simulated_datasets/Coverage_dataset/Run_Coverage.R
@@ -0,0 +1,17 @@
+if (!requireNamespace("ViralBottleneck", quietly = TRUE)) {
+  # devtools is heavier; remotes works too
+  if (!requireNamespace("devtools", quietly = TRUE))
+    install.packages("devtools")          # only once
+  devtools::install_github("BowenArchaman/ViralBottleneck",
+                           dependencies = TRUE,   # pull CRAN deps
+                           upgrade = "never")     # don’t re-upgrade installed deps
+}
+library(ViralBottleneck)
+setwd("/home3/2748683z/R_pub/First_simulation/CoverageDataset_noerror/R_input")
+transmission_pairs = read.table("Coverage_transmission_pairs.csv",header = TRUE,sep = ",")
+H1N1_ob = ViralBottleneck::CreateTransmissionObject(transmission_pairs)
+KL_output = ViralBottleneck::Bottleneck_size_Calculation(H1N1_ob, method = "KL",variant_calling = 0.01,Nbmin=1,Nbmax=400,donor_depth_threshold =0,recipient_depth_threshold = 0,error_filtering = 0,show_table =TRUE)
+PA_output= ViralBottleneck::Bottleneck_size_Calculation(H1N1_ob, method = "Presence-Absence",variant_calling = 0.01,Nbmin=1,Nbmax=400,donor_depth_threshold =0,recipient_depth_threshold = 0,error_filtering = 0,show_table =TRUE)
+App=ViralBottleneck::Bottleneck_size_Calculation(H1N1_ob, method = "Beta_binomial_Approximate",variant_calling = 0.01,Nbmin=1,Nbmax=400,donor_depth_threshold =0,recipient_depth_threshold = 0,error_filtering = 0,show_table =TRUE)
+Exact=ViralBottleneck::Bottleneck_size_Calculation(H1N1_ob, method = "Beta_binomial_Exact",variant_calling = 0.01,Nbmin=1,Nbmax=400,donor_depth_threshold =0,recipient_depth_threshold = 0,error_filtering = 0,show_table =TRUE)
+binomial=ViralBottleneck::Bottleneck_size_Calculation(H1N1_ob, method = "Binomial",variant_calling = 0.01,Nbmin=1,Nbmax=400,donor_depth_threshold =0,recipient_depth_threshold = 0,error_filtering = 0,show_table =TRUE)
